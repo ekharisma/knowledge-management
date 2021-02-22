49097540cc1d80dcbfce690d2196db8f42d78617
ekharisma <ekharisma@protonmail.com>
2021-02-22 07:44:02 +0700

    grafik jumlah divisi

--- dist/js/dashboard.js
+++ dist/js/dashboard.js
@@ -2,8 +2,7 @@ document.getElementById("jumlah_dokumen").innerHTML = berkas.length;
 document.getElementById("jumlah_pengguna").innerHTML = pengguna.length;
 let data = [];
 for (let index = 0; index < dataPerBulan.length; index++) {
-      console.log(dataPerBulan[index].jumlah);
-      data.push(dataPerBulan[index].jumlah);
+    data.push(dataPerBulan[index].jumlah);
 }
 var _ctx5 = cash('#grafik_jumlah_dokumen')[0].getContext('2d');
 // Grafik jumlah dokumen
@@ -16,10 +15,64 @@ var _myChart = new Chart(_ctx5, {
         datasets: [{
             label: 'Jumlah Dokumen',
             barPercentage: 0.5,
-            barThickness: 10,
-            maxBarThickness: 8,
+            barThickness: 15,
+            maxBarThickness: 20,
             minBarLength: 2,
             data: data,
+            backgroundColor: '#3160D8'
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                ticks: {
+                    fontSize: '12',
+                    fontColor: '#777777'
+                },
+                gridLines: {
+                    display: false
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    fontSize: '12',
+                    fontColor: '#777777',
+                    callback: function callback(value, index, values) {
+                        return value;
+                    }
+                },
+                gridLines: {
+                    color: '#D8D8D8',
+                    zeroLineColor: '#D8D8D8',
+                    borderDash: [2, 2],
+                    zeroLineBorderDash: [2, 2],
+                    drawBorder: false
+                }
+            }]
+        }
+    }
+});
+
+var DokumenPerDivisi = cash('#grafik_jumlah_dokumen_per_divisi')[0].getContext('2d');
+// Grafik jumlah dokumen
+// data dokumen per bulan
+let jumlahDokumen = [];
+let divisi = [];
+for (let index = 0; index < dataPerDivisi.length; index++) {
+    jumlahDokumen.push(dataPerDivisi[index].jumlahdokumen);
+    divisi.push(dataPerDivisi[index].divisi);
+}
+var grafikDokumenPerDivisi = new Chart(DokumenPerDivisi, {
+    type: 'bar',
+    data: {
+        labels: divisi,
+        datasets: [{
+            label: 'Jumlah Dokumen',
+            barPercentage: 0.5,
+            barThickness: 15,
+            maxBarThickness: 20,
+            minBarLength: 2,
+            data: jumlahDokumen,
             backgroundColor: '#3160D8'
         }]
     },
--- pages/dashboard.php
+++ pages/dashboard.php
@@ -12,10 +12,12 @@ $result = $mysqli->query($sql);
 $pengguna = $converter->toJson($result);
 $sql = "SELECT COUNT(*) as jumlah FROM tb_berkas WHERE tanggal LIKE ?";
 $stmt = $mysqli->prepare($sql);
-$bulan = ['2020-01%', '2020-02%', '2020-03%', '2020-04%', '2020-5%', '2020-06%', '2020-07%', '2020-08%',
-            '2020-09%', '2020-10%', '2020-11%', '2020-12%'];
+$bulan = [
+    '2020-01%', '2020-02%', '2020-03%', '2020-04%', '2020-5%', '2020-06%', '2020-07%', '2020-08%',
+    '2020-09%', '2020-10%', '2020-11%', '2020-12%'
+];
 $dataPerBulan = array();
-for ($i=0; $i <= 12; $i++) { 
+for ($i = 0; $i <= 12; $i++) {
     # code...
     $tempBulan = $bulan[$i];
     $stmt->bind_param('s', $tempBulan);
@@ -23,8 +25,17 @@ for ($i=0; $i <= 12; $i++) {
     $data = $stmt->get_result()->fetch_assoc();
     $dataPerBulan[$i] = $data;
 }
-
+//dokumen per divisi
+$sql = "SELECT tb_divisi.id_divisi, tb_divisi.divisi, tb_pengguna.id_pengguna, tb_pengguna.id_divisi, tb_berkas.id_pengguna,
+COUNT(divisi) AS jumlahdokumen
+FROM tb_divisi, tb_pengguna, tb_berkas
+WHERE tb_divisi.id_divisi = tb_pengguna.id_divisi
+AND tb_pengguna.id_pengguna = tb_berkas.id_pengguna
+GROUP BY divisi";
+$result = $mysqli->query($sql);
+$dokumenPerdivisi = $converter->toJson($result);
 ?>
+
 <!DOCTYPE html>
 <!--
 Template Name: Midone - HTML Admin Dashboard Template
@@ -1232,125 +1243,98 @@ License: You must have a valid license purchased only from themeforest(the above
                     </div>
                     <!-- END: General Report -->
                     <!-- BEGIN: Sales Report -->
-                    <div class="col-span-12 lg:col-span-6 mt-8">
+                    <div class="col-span-12 lg:col-span-12 mt-8">
                         <div class="intro-y block sm:flex items-center h-10">
                             <h2 class="text-lg font-medium truncate mr-5">
                                 Jumlah Dokumen Per Bulan
                             </h2>
                         </div>
                         <div class="intro-y box p-5 mt-12 sm:mt-5">
-                            <div class="flex flex-col xl:flex-row xl:items-center">
-                                <div class="flex">
-                                    <div>
-                                        <div class="text-theme-20 dark:text-gray-300 text-lg xl:text-xl font-bold">$15,000</div>
-                                        <div class="text-gray-600 dark:text-gray-600">This Month</div>
-                                    </div>
-                                    <div class="w-px h-12 border border-r border-dashed border-gray-300 dark:border-dark-5 mx-4 xl:mx-6"></div>
-                                    <div>
-                                        <div class="text-gray-600 dark:text-gray-600 text-lg xl:text-xl font-medium">$10,000</div>
-                                        <div class="text-gray-600 dark:text-gray-600">Last Month</div>
-                                    </div>
-                                </div>
-                                <div class="dropdown xl:ml-auto mt-5 xl:mt-0">
-                                    <button class="dropdown-toggle button font-normal border dark:border-dark-5 text-white dark:text-gray-300 relative flex items-center text-gray-700"> Filter by Category <i data-feather="chevron-down" class="w-4 h-4 ml-2"></i> </button>
-                                    <div class="dropdown-box w-40">
-                                        <div class="dropdown-box__content box dark:bg-dark-1 p-2 overflow-y-auto h-32"> <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md">PC & Laptop</a> <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md">Smartphone</a> <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md">Electronic</a> <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md">Photography</a> <a href="" class="flex items-center block p-2 transition duration-300 ease-in-out bg-white dark:bg-dark-1 hover:bg-gray-200 dark:hover:bg-dark-2 rounded-md">Sport</a> </div>
-                                    </div>
-                                </div>
-                            </div>
                             <div class="report-chart">
-                                <canvas id="grafik_jumlah_dokumen" height="200" class="mt-6"></canvas>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- END: Sales Report -->
-                    <!-- BEGIN: Weekly Top Seller -->
-                    <div class="col-span-12 sm:col-span-6 lg:col-span-3 mt-8">
-                        <div class="intro-y flex items-center h-10">
-                            <h2 class="text-lg font-medium truncate mr-5">
-                                <p>Dokumen per Divisi</p>
-                            </h2>
-                        </div>
-                        <div class="intro-y box p-5 mt-5">
-                            <canvas class="mt-3" id="report-pie-chart" height="280"></canvas>
-                            <div class="mt-8">
-                                <div class="flex items-center">
-                                    <div class="w-2 h-2 bg-theme-11 rounded-full mr-3"></div>
-                                    <span class="truncate">17 - 30 Years old</span>
-                                    <div class="h-px flex-1 border border-r border-dashed border-gray-300 mx-3 xl:hidden"></div>
-                                    <span class="font-medium xl:ml-auto">62%</span>
-                                </div>
-                                <div class="flex items-center mt-4">
-                                    <div class="w-2 h-2 bg-theme-1 rounded-full mr-3"></div>
-                                    <span class="truncate">31 - 50 Years old</span>
-                                    <div class="h-px flex-1 border border-r border-dashed border-gray-300 mx-3 xl:hidden"></div>
-                                    <span class="font-medium xl:ml-auto">33%</span>
-                                </div>
-                                <div class="flex items-center mt-4">
-                                    <div class="w-2 h-2 bg-theme-12 rounded-full mr-3"></div>
-                                    <span class="truncate">>= 50 Years old</span>
-                                    <div class="h-px flex-1 border border-r border-dashed border-gray-300 mx-3 xl:hidden"></div>
-                                    <span class="font-medium xl:ml-auto">10%</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- END: Weekly Top Seller -->
-                    <!-- BEGIN: Sales Report -->
-                    <div class="col-span-12 sm:col-span-6 lg:col-span-3 mt-8">
-                        <div class="intro-y flex items-center h-10">
-                            <h2 class="text-lg font-medium truncate mr-5">
-                                Sales Report
-                            </h2>
-                            <a href="" class="ml-auto text-theme-1 dark:text-theme-10 truncate">See all</a>
-                        </div>
-                        <div class="intro-y box p-5 mt-5">
-                            <canvas class="mt-3" id="report-donut-chart" height="280"></canvas>
-                            <div class="mt-8">
-                                <div class="flex items-center">
-                                    <div class="w-2 h-2 bg-theme-11 rounded-full mr-3"></div>
-                                    <span class="truncate">17 - 30 Years old</span>
-                                    <div class="h-px flex-1 border border-r border-dashed border-gray-300 mx-3 xl:hidden"></div>
-                                    <span class="font-medium xl:ml-auto">62%</span>
-                                </div>
-                                <div class="flex items-center mt-4">
-                                    <div class="w-2 h-2 bg-theme-1 rounded-full mr-3"></div>
-                                    <span class="truncate">31 - 50 Years old</span>
-                                    <div class="h-px flex-1 border border-r border-dashed border-gray-300 mx-3 xl:hidden"></div>
-                                    <span class="font-medium xl:ml-auto">33%</span>
-                                </div>
-                                <div class="flex items-center mt-4">
-                                    <div class="w-2 h-2 bg-theme-12 rounded-full mr-3"></div>
-                                    <span class="truncate">>= 50 Years old</span>
-                                    <div class="h-px flex-1 border border-r border-dashed border-gray-300 mx-3 xl:hidden"></div>
-                                    <span class="font-medium xl:ml-auto">10%</span>
-                                </div>
+                                <canvas id="grafik_jumlah_dokumen" height="100" class="mt-6"></canvas>
                             </div>
                         </div>
                     </div>
                     <!-- END: Sales Report -->
                     <!-- BEGIN: Official Store -->
-                    <div class="col-span-12 xl:col-span-8 mt-6">
+                    <div class="col-span-12 lg:col-span-12 mt-8">
                         <div class="intro-y block sm:flex items-center h-10">
                             <h2 class="text-lg font-medium truncate mr-5">
-                                Official Store
+                                Jumlah Dokumen Per Divisi
                             </h2>
-                            <div class="sm:ml-auto mt-3 sm:mt-0 relative text-gray-700 dark:text-gray-300">
-                                <i data-feather="map-pin" class="w-4 h-4 z-10 absolute my-auto inset-y-0 ml-3 left-0"></i>
-                                <input type="text" class="input w-full sm:w-40 box pl-10" placeholder="Filter by city">
-                            </div>
                         </div>
                         <div class="intro-y box p-5 mt-12 sm:mt-5">
-                            <div>250 Official stores in 21 countries, click the marker to see location details.</div>
-                            <div class="report-maps mt-5 bg-gray-200 rounded-md" data-center="-6.2425342, 106.8626478" data-sources="/dist/json/location.json"></div>
+                            <div class="report-chart">
+                                <canvas id="grafik_jumlah_dokumen_per_divisi" height="100" class="mt-6"></canvas>
+                            </div>
                         </div>
                     </div>
                     <!-- END: Official Store -->
                     <!-- BEGIN: Weekly Best Sellers -->
-                    <div class="col-span-12 xl:col-span-4 mt-6">
+                    <div class="col-span-12 xl:col-span-6 mt-6">
                         <div class="intro-y flex items-center h-10">
                             <h2 class="text-lg font-medium truncate mr-5">
-                                Weekly Best Sellers
+                                <p>Last Upload</p>
+                            </h2>
+                        </div>
+                        <div class="mt-5">
+                            <div class="intro-y">
+                                <div class="box px-4 py-4 mb-3 flex items-center zoom-in">
+                                    <div class="w-10 h-10 flex-none image-fit rounded-md overflow-hidden">
+                                        <img alt="Midone Tailwind HTML Admin Template" src="../dist/images/profile-15.jpg">
+                                    </div>
+                                    <div class="ml-4 mr-auto">
+                                        <div class="font-medium">Brad Pitt</div>
+                                        <div class="text-gray-600 text-xs">23 March 2021</div>
+                                    </div>
+                                    <div class="py-1 px-2 rounded-full text-xs bg-theme-9 text-white cursor-pointer font-medium">137 Sales</div>
+                                </div>
+                            </div>
+                            <div class="intro-y">
+                                <div class="box px-4 py-4 mb-3 flex items-center zoom-in">
+                                    <div class="w-10 h-10 flex-none image-fit rounded-md overflow-hidden">
+                                        <img alt="Midone Tailwind HTML Admin Template" src="../dist/images/profile-10.jpg">
+                                    </div>
+                                    <div class="ml-4 mr-auto">
+                                        <div class="font-medium">Brad Pitt</div>
+                                        <div class="text-gray-600 text-xs">20 June 2020</div>
+                                    </div>
+                                    <div class="py-1 px-2 rounded-full text-xs bg-theme-9 text-white cursor-pointer font-medium">137 Sales</div>
+                                </div>
+                            </div>
+                            <div class="intro-y">
+                                <div class="box px-4 py-4 mb-3 flex items-center zoom-in">
+                                    <div class="w-10 h-10 flex-none image-fit rounded-md overflow-hidden">
+                                        <img alt="Midone Tailwind HTML Admin Template" src="../dist/images/profile-13.jpg">
+                                    </div>
+                                    <div class="ml-4 mr-auto">
+                                        <div class="font-medium">Angelina Jolie</div>
+                                        <div class="text-gray-600 text-xs">27 June 2021</div>
+                                    </div>
+                                    <div class="py-1 px-2 rounded-full text-xs bg-theme-9 text-white cursor-pointer font-medium">137 Sales</div>
+                                </div>
+                            </div>
+                            <div class="intro-y">
+                                <div class="box px-4 py-4 mb-3 flex items-center zoom-in">
+                                    <div class="w-10 h-10 flex-none image-fit rounded-md overflow-hidden">
+                                        <img alt="Midone Tailwind HTML Admin Template" src="../dist/images/profile-11.jpg">
+                                    </div>
+                                    <div class="ml-4 mr-auto">
+                                        <div class="font-medium">Kate Winslet</div>
+                                        <div class="text-gray-600 text-xs">20 April 2021</div>
+                                    </div>
+                                    <div class="py-1 px-2 rounded-full text-xs bg-theme-9 text-white cursor-pointer font-medium">137 Sales</div>
+                                </div>
+                            </div>
+                            <a href="" class="intro-y w-full block text-center rounded-md py-4 border border-dotted border-theme-15 dark:border-dark-5 text-theme-16 dark:text-gray-600">View More</a>
+                        </div>
+                    </div>
+                    <!-- END: Weekly Best Sellers -->
+                                        <!-- BEGIN: Weekly Best Sellers -->
+                                        <div class="col-span-12 xl:col-span-6 mt-6">
+                        <div class="intro-y flex items-center h-10">
+                            <h2 class="text-lg font-medium truncate mr-5">
+                                <p>Most Viewed</p>
                             </h2>
                         </div>
                         <div class="mt-5">
@@ -1942,6 +1926,8 @@ License: You must have a valid license purchased only from themeforest(the above
         var berkas = <?= $berkas ?>;
         var pengguna = <?= $pengguna ?>;
         var dataPerBulan = <?= json_encode($dataPerBulan) ?>;
+        var dataPerDivisi = <?= $dokumenPerdivisi ?>;
+        console.log(dataPerDivisi);
     </script>
     <script src="../dist//js//dashboard.js"></script>
     <!-- END: JS Assets-->
